6d0ec1bfd6d525ee51a579eb2b453170369aa241
Trinity Lundgren <trinity@trinitylundgren.com>
2021-02-14 18:51:44 -0800

    Add SELECT and INSERT queries for Customers and Vets

--- hwp_dml.sql
+++ hwp_dml.sql
@@ -9,8 +9,13 @@
 -- Classes page: search for a class by class name, class day, class time or class price
 
 -- Vets page: search for a vet by first name, last name, specialty or pet name
+SELECT * FROM vets WHERE first_name = :firstName
+SELECT * FROM vets WHERE last_name = :lastName
+SELECT * FROM vets WHERE specialty = :specialty
+SELECT * FROM vets WHERE vet_id = (SELECT vet_id from pets where pet_name = :petName)
 
 -- Admin page: show all Customers
+SELECT * from customers
 
 -- Admin page: show all Pets
 
@@ -19,18 +24,22 @@
 -- Admin page: show all Enrollments
 
 -- Admin page: show all Vets
+SELECT * from vets
 
 --
 -- INSERT queries
 --
 
 -- Customers page: register a new customer
+INSERT INTO customers (first_name, last_name, email, phone, address, city, state, zip_code) VALUES (:firstName, :lastName, :email, :phone, :address, :city, :state, :zip)
 
 -- Customers page: add a pet
+INSERT INTO pets (pet_name, species, breed, age, gender, vet_id, customer_id) VALUES (:petName, :petSpecies, :petBreed, :petAge, :vetId, :customerId)
 
 -- Classes page: enroll a pet in a class by ID
 
 -- Admin page: add Customer
+INSERT INTO customers (first_name, last_name, email, phone, address, city, state, zip_code) VALUES (:firstName, :lastName, :email, :phone, :address, :city, :state, :zip)
 
 -- Admin page: add Pet
 
@@ -39,6 +48,7 @@
 -- Admin page: add Enrollment
 
 -- Admin page: add Vet
+INSERT INTO vets (first_name, last_name, email, phone, specialty) VALUES (:firstName, :lastName, :email, :phone, :specialty)
 
 --
 -- UPDATE queries

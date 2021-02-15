0e208f5eec2c204f5b741f1e372e7bfb641c8073
Trinity Lundgren <trinity@trinitylundgren.com>
2021-02-14 18:25:54 -0800

    Rename ddl file, add dml file with comment outline

---
+++ hwp_dml.sql
@@ -0,0 +1,69 @@
+-- DML queries for Hello World Pets
+
+--
+-- SELECT queries
+--
+
+-- Pets page: search for a pet by name, pet name, or pet type
+
+-- Classes page: search for a class by class name, class day, class time or class price
+
+-- Vets page: search for a vet by first name, last name, specialty or pet name
+
+-- Admin page: show all Customers
+
+-- Admin page: show all Pets
+
+-- Admin page: show all Classes
+
+-- Admin page: show all Enrollments
+
+-- Admin page: show all Vets
+
+--
+-- INSERT queries
+--
+
+-- Customers page: register a new customer
+
+-- Customers page: add a pet
+
+-- Classes page: enroll a pet in a class by ID
+
+-- Admin page: add Customer
+
+-- Admin page: add Pet
+
+-- Admin page: add Class
+
+-- Admin page: add Enrollment
+
+-- Admin page: add Vet
+
+--
+-- UPDATE queries
+--
+
+-- Admin page: update Customer
+
+-- Admin page: update Pet
+
+-- Admin page: update Class
+
+-- Admin page: update Enrollment
+
+-- Admin page: update Vet
+
+-- 
+-- DELETE queries
+--
+
+-- Admin page: delete Customer
+
+-- Admin page: delete Pet
+
+-- Admin page: delete Class
+
+-- Admin page: delete Enrollment
+
+-- Admin page: delete Vet
